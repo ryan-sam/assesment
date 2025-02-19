219a6fa03a8f3c33f2bf71c4f17448c62a24cc29
ryan-sam <munyiryan8@gmail.com>
2025-02-19 15:21:16 +0300

    Update functionals.py

--- functionals.py
+++ functionals.py
@@ -1,9 +1,3 @@
-def calculateage(name,age):
-    from datetime import datetime
-    current_year = datetime.now.year
-    age = current_year - birth_year
-    return (f"hello{name} your age is ")
-user_name = int(input("Enter age: "))
-user_birth_year = int(input("Enter your date of birth"))
-result = calculateage(user_name, user_birth_year)
-print(result)
+name = int(input("ENTER YOUR NAME: "))
+age = int(input("ENTER YOUR AGE: "))
+print("Hello{name} your age is {age}" )
